79781ae0048788132e5407129786a15acd424cdf
Sherushe <jyburkhard@gmail.com>
2014-11-10 23:55:31 +0100

    deg to rad

--- Tank Game/Bullet.cpp
+++ Tank Game/Bullet.cpp
@@ -17,8 +17,8 @@ Bullet::~Bullet()
 void Bullet::Update(RenderWindow* _window, float _elapsedTime)
 {
 	/// \TODO: Why aren't the bullets moving?
-	const int nextPosX = getPosition().x + cosf(getRotation()) * velocity * _elapsedTime,
-			  nextPosY = getPosition().y + sinf(getRotation()) * velocity * _elapsedTime;
+	const int nextPosX = getPosition().x + cosf(DEG2RAD(getRotation()) + PI/2) * velocity * _elapsedTime,
+			  nextPosY = getPosition().y + sinf(DEG2RAD(getRotation()) + PI/2) * velocity * _elapsedTime;
 	setPosition(nextPosX, nextPosY);
 
 	Render(_window);
